4904ce2fc2d7d5943cdda034a98053ec58ce3bac
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-08-26 23:28:21 +0530

    final view.

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,12 +1,17 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img")
 
 const updateUI = (data) => {
-  const cityDets = data.cityDets;
-  const weather = data.weather;
+//   const cityDets = data.cityDets;
+//   const weather = data.weather;
 
   console.log(data);
+
+  //destructure properties
+  const {cityDets,weather} = data;
 
 //   update details template
     details.innerHTML = `
@@ -16,6 +21,18 @@ const updateUI = (data) => {
           <span>${weather.Temperature.Metric.Value}</span>
           <span>&deg;C</span>
       </div>`;
+
+      // update the night/day & icon images
+      const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+      icon.setAttribute("src",iconSrc);
+      
+      let timeSrc = null;
+      if(weather.IsDayTime){
+        timeSrc ="img/day.svg";
+      }else{
+        timeSrc="img/night.svg";
+      }
+      time.setAttribute("src",timeSrc);
 
       // remove the d-none class if present
       if(card.classList.contains("d-none")){
