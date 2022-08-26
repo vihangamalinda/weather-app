b1ce3b70bf15513c5dc6239f4b324e272229f98b
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-08-26 11:45:52 +0530

    Get City API call.

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,16 @@
+const key ="fHaPBBJy9lAM5Ejf8YoIUpeIH5vMvyJI";
+
+const getCity = async (city) =>{
+
+    const base ="http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query =`?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+
+getCity("manchester").then(data=>console.log(data)).catch(err => console.log(err));
