dc1dfd84dd5ce23d29ddff5b557b31aa2bd94bac
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-08-26 12:05:32 +0530

    get Weather API call.

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,5 +1,18 @@
 const key ="fHaPBBJy9lAM5Ejf8YoIUpeIH5vMvyJI";
 
+// get weather information
+const getWeather =  async(id) =>{
+    const base  = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const  data = await response.json();
+
+    return data[0];
+
+};
+
+//  get city information
 const getCity = async (city) =>{
 
     const base ="http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -13,4 +26,13 @@ const getCity = async (city) =>{
 }
 
 
-getCity("manchester").then(data=>console.log(data)).catch(err => console.log(err));
+getCity("manchester")
+.then(data => {
+    return getWeather(data.Key);
+})
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
+// getWeather(329260)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
