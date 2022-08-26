1259250b5bf4e7a03ef6a0cebb714ceb7c86d13e
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-08-26 14:03:27 +0530

    Updating the UI.

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,25 +1,48 @@
 const cityForm = document.querySelector("form");
-const updateCity = async (city) =>{
-    // console.log(city);
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 
-    return {
-        cityDets:cityDets,
-        weather:weather
-    }
-}
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
 
-cityForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
+  console.log(data);
 
-    // get City value
-    const city  = cityForm.city.value.trim();
-    cityForm.reset();
+//   update details template
+    details.innerHTML = `
+     <h5 class="my-3">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+      </div>`;
 
-    // update the UI with new city
-    updateCity(city)
-    .then(data =>console.log(data))
-    .catch(err =>console.log(err));
+      // remove the d-none class if present
+      if(card.classList.contains("d-none")){
+        card.classList.remove("d-none");
+      }
+};
 
-})
+const updateCity = async (city) => {
+  // console.log(city);
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {
+    cityDets: cityDets,
+    weather: weather,
+  };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get City value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the UI with new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
